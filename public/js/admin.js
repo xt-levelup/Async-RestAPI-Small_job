535cae80cbfbff38ea800638d44315b67ed12198
Phan Minh Cuong <xitrumvndn5@gmail.com>
2024-03-29 09:31:10 +0700

    Changed products.ejs in admin. Add admin.js into js folder.

---
+++ public/js/admin.js
@@ -0,0 +1,4 @@
+const deleteProduct = (btn) => {
+    const productId = btn.parentNode.querySelector("[name=productId]").value;
+    const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
+};
